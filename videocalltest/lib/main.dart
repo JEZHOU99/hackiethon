3a1f57a4b3c1a05c425079c5de99da042a033e24
JEZHOU99 <81694589+JEZHOU99@users.noreply.github.com>
2021-04-03 12:48:06 +1100

    main

---
+++ videocalltest/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:videocalltest/HomePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Agora Group Video Call',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.blueAccent,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
---
+++ videocalltest/lib/utils.dart
@@ -0,0 +1,1 @@
+const appID = '30c86490d3e043f1a0ec9aa120b29a7a';
